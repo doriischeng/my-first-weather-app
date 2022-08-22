f211265db176a05977884fef6d5ff6b5b27ee3f0
dorisc712 <106878555+dorisc712@users.noreply.github.com>
2022-08-22 23:57:16 +0800

    Displayed weather information and time for city using API

---
+++ src/app.js
@@ -0,0 +1,72 @@
+function formattedDate(timestamp) {
+  let now = new Date(timestamp);
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let day = days[now.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector(
+    "#temperature"
+  );
+  temperatureElement.innerHTML = Math.round(
+    response.data.main.temp
+  );
+  let cityElement =
+    document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+
+  let humidityElement =
+    document.querySelector("#humidity");
+  humidityElement.innerHTML =
+    response.data.main.humidity;
+
+  let windElement =
+    document.querySelector("#wind");
+  windElement.innerHTML = Math.round(
+    response.data.wind.speed
+  );
+
+  let descriptionElement = document.querySelector(
+    "#description"
+  );
+  descriptionElement.innerHTML =
+    response.data.weather[0].description;
+
+  let iconElement =
+    document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+
+  let dateElement =
+    document.querySelector("#date");
+  dateElement.innerHTML = formattedDate(
+    response.data.dt * 1000
+  );
+}
+
+let apiKey = "4a57d92459ebaebf0556db5aa7e8c670";
+let city = "Paris";
+let units = "metric";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+axios.get(apiUrl).then(displayTemperature);
